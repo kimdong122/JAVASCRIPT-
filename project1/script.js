edadd806178e94f318f5f5b497c09937b4289160
Your Name <you@example.com>
2025-08-25 23:52:52 +0900

    자바스크립트 기초

---
+++ project1/script.js
@@ -0,0 +1,17 @@
+let myName = "김동혁";
+let myAge = 25;
+let isLearning = true;
+
+let nickName = "코딩";
+let hobby = "운동하기";
+let goal = "프론트엔드 개발자 ";
+
+console.log("✅ 자바스크립트 공부 시작!");
+console.log("이름:", myName);
+console.log("나이:", myAge);
+console.log("공부중 이신가요?", isLearning);
+
+console.log("🔥 자기소개");
+console.log("별명:", nickName);
+console.log("취미:", hobby);
+console.log("목표:", goal);
